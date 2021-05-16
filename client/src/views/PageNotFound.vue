57a7faf22350f82af1dc58a5d67a598078e3d838
zaini14 <zainiazhar51@gmail.com>
2021-05-16 09:32:15 +0700

    menambahkan file page not found dan detail emplooye

---
+++ client/src/views/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<p>Halaman Tidak Ditemukan</p>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'PageNotFound',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
